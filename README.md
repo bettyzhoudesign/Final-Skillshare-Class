99b76005839560789832e93ae34b91317327697e
Betty Z <bettyzhoudesign@gmail.com>
2024-10-01 14:41:56 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Final-Skillshare-Class
+ 
---
+++ src/script.js
@@ -0,0 +1,11 @@
+console.log("hey there!");
+
+function showdialog() {
+  var dialog = document.getElementById("dialog");
+  dialog.classList.add("show");
+}
+
+function hidedialog() {
+  var dialog = document.getElementById("dialog");
+  dialog.classList.remove("show");
+}
